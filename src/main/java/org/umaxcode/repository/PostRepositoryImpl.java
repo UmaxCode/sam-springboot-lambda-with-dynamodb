4a31ad7d855bd60c20a9cff34bb090849abc3119
umaxcode <maxwellodoom1729@gmail.com>
2024-12-25 19:11:41 +0000

    refactor the update function

--- src/main/java/org/umaxcode/repository/PostRepositoryImpl.java
+++ src/main/java/org/umaxcode/repository/PostRepositoryImpl.java
@@ -1,10 +1,14 @@
 package org.umaxcode.repository;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.umaxcode.domain.entity.Post;
+import org.umaxcode.exception.ServiceException;
 
 import java.util.List;
 
@@ -27,22 +31,26 @@ public class PostRepositoryImpl implements PostRepository {
 
     @Override
     public Post getPostById(String postId) {
-        return dynamoDBMapper.load(Post.class, postId);
+        Post post = dynamoDBMapper.load(Post.class, postId);
+        if (post != null) {
+            return post;
+        }
+
+        throw new ServiceException("Post with id " + postId + " not found");
     }
 
     @Override
     public Post updatePost(String postId, Post post) {
-
-        Post post1 = getPostById(postId);
-        post1.setTitle(post.getTitle() != null ? post.getTitle() : post1.getTitle());
-        post1.setContent(post.getContent() != null ? post.getContent() : post1.getContent());
-        dynamoDBMapper.save(post1);
+        dynamoDBMapper.save(post, new DynamoDBSaveExpression()
+                .withExpectedEntry("id", new ExpectedAttributeValue(
+                        new AttributeValue(postId)
+                )));
         return null;
     }
 
     @Override
     public void deletePost(String postId) {
-       Post post = getPostById(postId);
-       dynamoDBMapper.delete(post);
+        Post post = getPostById(postId);
+        dynamoDBMapper.delete(post);
     }
 }
